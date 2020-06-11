64a33fd3bf5493f93355a9fe5917caf5215d48d0
Felix Wippich <felix.wippich@me.com>
2020-06-11 18:32:22 +0200

    Fix pllugin.js

--- config/env/production/plugin.js
+++ config/env/production/plugin.js
@@ -2,10 +2,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "cloudinary",
     providerOptions: {
-      'cloud_name': process.env.CLOUDINARY_CLOUD_NAME,
-      'api_secret': process.env.CLOUDINARY_API_SECRET,
-      'api_key': process.env.CLOUDINARY_API_KEY,
-      },
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      api_key: process.env.CLOUDINARY_API_KEY,
     },
   },
 });
